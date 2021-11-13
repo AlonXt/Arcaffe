c698534ccbfdb5d4c2893fcd8c46259c87cfda98
Alon <alon.xt@gmail.com>
2021-11-13 19:48:19 +0200

    deleted datetime stuff

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from get_data_from_10bis import main
-import datetime, uvicorn
+import uvicorn
 #  uvicorn api:app --reload
 
 def run():
@@ -7,15 +7,7 @@ def run():
     uvicorn.run("api:app", host="127.0.0.1", port=8000, log_level="info")
 
 
-# def refresh_menu_data(START_DATE:datetime.date):
-#     today = datetime.date.today()
-#     if today > START_DATE:
-#         main()
-
 if __name__ == '__main__':
-    # START_DATE = datetime.date(2021, 11, 13)
-    # today = datetime.date.today()
-
     try:
         run()
     except Exception as e:
